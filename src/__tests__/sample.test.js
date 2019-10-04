57d999a2da420cc0a835d42e3e6437bf341ab264
awaji-daisuke <awaji_daisuke@intec.co.jp>
2019-10-04 10:22:50 +0900

    Update sample.test.js

--- src/__tests__/sample.test.js
+++ src/__tests__/sample.test.js
@@ -3,7 +3,7 @@ const hello = require('../main/hello');
 const Clazz = require('../main/classUsage');
 
 test('1 + 2 は 3', () => {
-    expect(sum(1, 2)).toBe(4);
+    expect(sum(1, 2)).toBe(3);
 });
 
 test('5 + 8 は 13', () => {
