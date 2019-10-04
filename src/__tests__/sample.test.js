899ccf583d9c73b79a1405c613fddfc2497a4ae5
awaji-daisuke <awaji_daisuke@intec.co.jp>
2019-10-04 11:24:52 +0900

    hello.jsをテストしたが、組み込み関数が間違っていたので修正した

--- src/__tests__/sample.test.js
+++ src/__tests__/sample.test.js
@@ -1,20 +1,33 @@
-const sum = require('../main/sum');
-const hello = require('../main/hello');
-const Clazz = require('../main/classUsage');
+const sum = require("../main/sum");
+const hello = require("../main/hello");
+const Clazz = require("../main/classUsage");
 
-test('1 + 2 は 3', () => {
-    expect(sum(1, 2)).toBe(3);
+test("1 + 2 は 3", () => {
+  expect(sum(1, 2)).toBe(3);
 });
 
-test('5 + 8 は 13', () => {
-    expect(sum(5, 8)).toBe(13);
+test("5 + 8 は 13", () => {
+  expect(sum(5, 8)).toBe(13);
 });
 
-test('-30 + -21 は -51', () => {
-    expect(sum(-30, -21)).toBe(-51);
+test("-30 + -21 は -51", () => {
+  expect(sum(-30, -21)).toBe(-51);
 });
 
-test('', () => {
-    let clazz = new Clazz(1, 2);
-    expect(clazz.value).toBe(3);
-}) 
+test("", () => {
+  let clazz = new Clazz(1, 2);
+  expect(clazz.value).toBe(3);
+});
+
+test("hello.jsのテスト hello to hello", () => {
+  expect(hello("hello")).toBe("hello");
+});
+test("hello.jsのテスト hey  to hello", () => {
+  expect(hello("hey")).toBe("hello");
+});
+test("hello.jsのテスト booooo to boy", () => {
+  expect(hello("booooo")).toBe("boy");
+});
+test("hello.jsのテスト else to yeah", () => {
+  expect(hello("ohmygod")).toBe("yeah");
+});
